b2af3e7f7ff47561cae018187059eaf119bb3507
Kayvon Fatahalian <kayvonf@myth58.stanford.edu>
2019-02-03 00:19:52 -0800

    Minor update.

--- common/contracts.h
+++ common/contracts.h
@@ -1,16 +1,12 @@
-/* Debugging with contracts; simulating cc0 -d
- * Enable with gcc -DDEBUG ...
- *
- * 15-122 Principles of Imperative Computation
- * Frank Pfenning
- */
-
 #include <assert.h>
 
 /* Unlike typical header files, "contracts.h" may be
  * included multiple times, with and without DEBUG defined.
  * For this to succeed we first undefine the macros in
  * question in order to avoid a redefinition warning.
+ *
+ * This file was borrowed from an original version developed by Frank
+ * Pfenning
  */
 
 #undef ASSERT
